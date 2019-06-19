6e0e03c78605d098cb64fba5fe7b8b2276a814c1
[slopez0286@gmail.com] <slopez0286@gmail.com>
2019-06-19 23:02:15 +0000

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print string using put
+ * Return:  0 to exit
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+
+return (0);
+}
