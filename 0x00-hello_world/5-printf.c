ca8f34e0d24e7c95f3481268e0f1cacb984059c1
[slopez0286@gmail.com] <slopez0286@gmail.com>
2019-06-19 23:13:27 +0000

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print string using put
+ * Return:  0 to exit
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
