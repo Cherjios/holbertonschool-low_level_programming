96f56873450eaf764b9979041722d6d244282e85
[slopez0286@gmail.com] <slopez0286@gmail.com>
2019-06-20 01:54:30 +0000

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - program that quote 
+*Return: 1 to exit
+*/
+
+int main(void)
+{
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
