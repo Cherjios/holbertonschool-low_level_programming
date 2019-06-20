90be94c0b73dfc0756b09c142cb086049b89f2aa
[slopez0286@gmail.com] <slopez0286@gmail.com>
2019-06-20 19:13:00 +0000

    Positive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 
 	if (n < 0)
 
-	printf("%i is negative\n", n);
+	printf("%d is negative\n", n);
 
 	else if (n == 0)
 
-	printf("%i is zero\n", n);
+	printf("%d is zero\n", n);
 
 	else
 
-	printf("%i is positive\n", n);
+	printf("%d is positive\n", n);
 return (0);
 
 }
