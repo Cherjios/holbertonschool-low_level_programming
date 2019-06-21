805b6c09bd099cac4f2125bd7b9a4d04973092f6
[slopez0286@gmail.com] <slopez0286@gmail.com>
2019-06-21 00:57:44 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Print all single digit numbers of base 10
+* Return: 0
+*/
+
+int main(void)
+{
+	int c;
+
+	c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
