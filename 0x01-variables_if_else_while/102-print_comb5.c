4498cce2608df053b2165f27f59ead40c0e1bb50
[slopez0286@gmail.com] <slopez0286@gmail.com>
2019-06-21 21:32:30 +0000

    Print all tow two digits comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers.
+
+
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n1, n2, n3, n4, p1, p2;
+
+	for (p1 = 0; p1 <= 99; p1++)
+	{
+		for (p2 = p1 + 1; p2 <= 99; p2++)
+		{
+			n1 = (p1 / 10) + 48;
+			n2 = (p1 % 10) + 48;
+			n3 = (p2 / 10) + 48;
+			n4 = (p2 % 10) + 48;
+
+			putchar(n1);
+			putchar(n2);
+			putchar(' ');
+			putchar(n3);
+			putchar(n4);
+
+			if (n1 != '9' || n2 != '8' || n3 != '9' || n4 != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
