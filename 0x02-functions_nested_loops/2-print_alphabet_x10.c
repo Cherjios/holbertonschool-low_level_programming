46f8526a2d193f6110a58c1a46f3fed325673260
[slopez0286@gmail.com] <slopez0286@gmail.com>
2019-06-26 01:49:49 +0000

    Alphabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,30 +1,10 @@
 #include "holberton.h"
-/**
-* main  - check the code for Holberton School students.
-* Return: Always 0.
- */
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
-
-/* #include "holberton.h" */
 
 /**
 *print_alphabet_x10 - Prints the alphabet, in lowercase, followed by a new line
 *10 times
 *Return: Always 0
 */
-
 
 void print_alphabet_x10(void)
 {
@@ -36,3 +16,4 @@ void print_alphabet_x10(void)
 		print_alphabet();
 		num++;
 	}
+}
