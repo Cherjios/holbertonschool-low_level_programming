6e0e651926db67e3ea9d777bdc31732bf3488f03
[slopez0286@gmail.com] <slopez0286@gmail.com>
2019-06-26 03:21:40 +0000

    Pritn 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,15 +10,38 @@
 
 void print_to_98(int n)
 {
-		while (n <= 98)
+	if (n < 98)
+	{
+		for (; n <= 98; n++)
 		{
-			printf("%d, ", n);
-			n++;
+			if (n != 98)
+			{
+
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d\n", n);
+			}
 		}
-		while (n > 98)
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else
+	{
+		for (; n >= 98; n--)
 		{
-			printf("%d, ", n);
-			n--;
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d\n", n);
+			}
 		}
+	}
 }
 
