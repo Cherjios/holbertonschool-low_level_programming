814e60df105a157b19bf93eba05602107a110bbd
[slopez0286@gmail.com] <slopez0286@gmail.com>
2019-06-26 00:28:28 +0000

    Prints Hello

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* main -Print Holberton
+*Return: Always 0
+*/
+
+int main(void)
+{
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
+	_putchar(10);
+
+return (0);
+
+}
