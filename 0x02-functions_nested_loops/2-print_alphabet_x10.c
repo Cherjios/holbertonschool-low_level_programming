31277a60ea4ffff75e92eb8cd2ae42c82a4efd25
[slopez0286@gmail.com] <slopez0286@gmail.com>
2019-06-26 02:22:01 +0000

    Alphabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,28 @@
 #include "holberton.h"
 
 /**
-*print_alphabet_x10 - Prints the alphabet, in lowercase, followed by a new line
-*10 times
+*print_alphabet - Prints the alphabet one time
+*print_alphabet_x10- Prints the alphabet ten times
 *Return: Always 0
 */
+
+void print_alphabet(void)
+{
+	int num;
+
+	num = 97;
+
+	while (num <= 122)
+	{
+	_putchar(num);
+
+	num++;
+
+	}
+
+	_putchar('\n');
+
+}
 
 void print_alphabet_x10(void)
 {
