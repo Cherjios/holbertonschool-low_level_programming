5b5be9e8b7474c0608ba1d26e46e963046dc38de
Sergio Lopez <slopez0286@gmail.com>
2019-06-28 20:57:43 +0000

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include"holberton.h"
+
+/**
+ * main - Finds and prints the largest prime factor of 
+ *the number 612852475143
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	long n;
+	
+	n = 612852475143;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+			i--;
+		}
+	}
+	printf("%d \n", i);
+		return (0);
+}
+
