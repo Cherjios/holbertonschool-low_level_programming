438c593edd696b553d8334a4d280c95bb2361613
Sergio Lopez <slopez0286@gmail.com>
2019-07-02 23:07:03 +0000

    Print array elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,15 +14,15 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n-1; i++)
+	for (i = 0; i <= n - 1; i++)
 	{
 
-		printf("%i",a[i]);
-		
+		printf("%i", a[i]);
+
 		if (i != n - 1)
 
 			printf(", ");
-		
+
 	}
 
 	printf("\n");
