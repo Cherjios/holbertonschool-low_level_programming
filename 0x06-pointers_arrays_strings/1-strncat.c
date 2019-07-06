78927f53d671bc3aa9a8221b90c034e12e3ab47b
Sergio Lopez <slopez0286@gmail.com>
2019-07-06 22:25:11 +0000

    Concatenates two strings n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+*_strncat-Function that concatenates two strings from n bytes
+*
+*@dest: is a chart value
+*@src: is a chart value
+*@n: is an int value
+*
+*Return:Returns a pointer to the resulting string dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	char *other = dest;
+
+	while (*dest)
+	{
+		dest++;
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+		*dest = '\0';
+
+
+	return (other);
+}
