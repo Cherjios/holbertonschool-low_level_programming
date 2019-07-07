f14d48bf918e3e7f8f6f15a160503894744890e4
Sergio Lopez <slopez0286@gmail.com>
2019-07-07 07:01:26 +0000

    Capitalizes all words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ **cap_string-Function that capitalizes all words of a string
+ *
+ *@s: is an char value
+ *
+ *Return:Char value
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';'
+|| s[i] == '.' || s[i] == '!'  || s[i] == '?' || s[i] == '"' || s[i] == '(' ||
+s[i] == ')' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+
+
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+
+	}
+	return (s);
+}
