a7bda726f3c1dd13c7585c46a7e5cdc2fd13cb50
Sergio Lopez <slopez0286@gmail.com>
2019-07-07 01:57:46 +0000

    Compare tow strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,17 +11,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
 
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
-
-	if (s1[i] != s2[i])
-
-		return (*s1 - *s2);
-
-		return (*s1 - *s2);
+	return (*s1 - *s2);
 
 }
