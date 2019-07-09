7d0f51d8bfa0b0e625b37892423890aedaf834e4
Sergio Lopez <slopez0286@gmail.com>
2019-07-09 19:58:30 +0000

    Copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+*_memcyp-Function that copies memory area.
+*
+*@dest: Is a chart value
+*@src: Is a chart value
+*@n: is an int value
+*
+*Return: a char value
+*/
+
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+
+		dest[i] = src[i];
+
+	return (dest);
+
+
+}
