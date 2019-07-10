73bd5c3998a9a7d560de77614f0633c1816b799e
Sergio Lopez <slopez0286@gmail.com>
2019-07-10 01:24:38 +0000

    sets the value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*set_string-Function that sets the value of pointer to a char
+*
+*@s:Is a chart value
+*@to: Is a chart value
+*
+*Return: a unsigned int
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
