8d1ff56d1925a73c099cca36b17c9676fbb4dbe8
Sergio Lopez <slopez0286@gmail.com>
2019-07-22 23:22:41 +0000

    Print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*print_dog-Function that concatenates two strings
+*
+*@d: dog sgruct
+*
+*Return: Always 0
+*/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+
+
+
+}
