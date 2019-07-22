783c14ea756b8b5d8b619cc6f4f34068812f0817
Sergio Lopez <slopez0286@gmail.com>
2019-07-22 21:24:57 +0000

    Dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog- Dog Structure
+*@name: Dog's name
+*@age: Dog's age
+*@owner: Dog's owner
+*
+*Description: Structure that fills dos information; name, age and woner.
+*
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
