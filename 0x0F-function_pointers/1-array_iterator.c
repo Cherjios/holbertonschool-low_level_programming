45cc08cb5b596307c74f52dcf4a03cdcf17180d9
Sergio Lopez <slopez0286@gmail.com>
2019-07-24 19:33:50 +0000

    Array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,6 +17,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 
-	for (n = 0; n <= size; n++)
+	for (n = 0; n < size; n++)
 		action(array[n]);
 }
