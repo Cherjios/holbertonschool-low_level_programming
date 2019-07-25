a83cbb7665f403981ad027925c0f35c6b450af88
Sergio Lopez <slopez0286@gmail.com>
2019-07-25 03:11:58 +0000

    Main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,13 +26,13 @@ argv[2][0] == '/' || argv[2][0] == '%'))
 		exit(99);
 	}
 
-		if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
-		{
-			printf("Error\n");
-			exit(100);
-		}
+	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-		printf("%i\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	printf("%i\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 
