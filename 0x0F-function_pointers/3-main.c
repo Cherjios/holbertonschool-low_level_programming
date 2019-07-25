4930589659be66a755ffa3609310c6499a0f9742
Sergio Lopez <slopez0286@gmail.com>
2019-07-25 02:58:09 +0000

    Main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+*main - Take the arguments in command line to perfome simple operations
+*
+*@argc: Number of argumets in the command line, Argumet count
+*@argv: Values to the operations placesl Argument vector
+*Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (!(argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '*' ||
+argv[2][0] == '/' || argv[2][0] == '%'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+		if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
+		{
+			printf("Error\n");
+			exit(100);
+		}
+
+		printf("%i\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+
+}
