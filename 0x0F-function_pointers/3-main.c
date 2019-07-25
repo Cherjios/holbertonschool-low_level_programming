9c4379f4b8c7c8fe77c49dc11ae9b338c742e0f2
Sergio Lopez <slopez0286@gmail.com>
2019-07-25 03:34:09 +0000

    Main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,13 +18,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-
-/*	if (!(argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '*' ||
-argv[2][0] == '/' || argv[2][0] == '%'))
+	if (argv[2][1] != '\0' || (get_op_func)(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
-	}*/
+	}
 
 	if ((argv[2][0] == '%' || argv[2][0] == '/') && argv[3][0] == '0')
 	{
