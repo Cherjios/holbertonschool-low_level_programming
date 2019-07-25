5d959396a05366642b75b7606aee4bf07a21f80c
Sergio Lopez <slopez0286@gmail.com>
2019-07-25 02:57:47 +0000

    Op functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+*op_add - function that return the sum of a and b
+*
+*@a: int value
+*@b: int value
+*Return: Sum of of two int
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+*op_sub - function that return the sub of a and b
+*
+*@a: int value
+*@b: int value
+*Return: Sub of of two int
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+*op_mul - function that return the mul of a and b
+*
+*@a: int value
+*@b: int value
+*Return: Mul of of two int
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+*op_div - function that return the div of a and b
+*
+*@a: int value
+*@b: int value
+*Return: div of of two int
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+*op_mod - function that return the mod of a and b
+*
+*@a: int value
+*@b: int value
+*Return: mod of of two int
+*/
+int op_mod(int a, int b)
+{
+
+	return (a % b);
+}
