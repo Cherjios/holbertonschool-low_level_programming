9f302878b485c1d5b67e946f2758691bc694f3f5
Sergio Lopez <slopez0286@gmail.com>
2019-09-13 17:41:08 +0000

    Print elements in list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+  *print_dlistint- Function that prints all elements of a list.
+  *@h: Takes in a const struct type *h
+  *Return: size_t counter value
+  */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		printf("%d \n", h->n);
+		h = h->next;
+		counter++;
+
+	}
+	return (counter);
+
+}
