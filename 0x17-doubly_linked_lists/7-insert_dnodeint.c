37dbe39b8e6b66513c3043f3e78e9e2da3a9f915
Sergio Lopez <slopez0286@gmail.com>
2019-09-15 06:47:10 +0000

    Insert 3

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,6 +39,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		node = node->next;
 		num++;
 	}
-		free(new_node);
-		return (node);
+	free(new_node);
+	return (node);
 }
