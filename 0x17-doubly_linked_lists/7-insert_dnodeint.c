6a22a49962a74a6e01d31f2d17bbd4f37e377260
Sergio Lopez <slopez0286@gmail.com>
2019-09-15 22:53:39 +0000

    Insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,6 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
+		new_node->prev = NULL;
 		new_node->next = *h;
 		*h = new_node;
 		return (new_node);
