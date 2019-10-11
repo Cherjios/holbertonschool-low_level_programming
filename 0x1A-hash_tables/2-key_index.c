575f2b867500f43f98dc60a5a48587654fdc28e3
Sergio Lopez <slopez0286@gmail.com>
2019-10-11 16:40:11 +0000

    Adding 2 key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ *key_index-Function that gives us the index of a key
+ *@key: Key to look for it 
+ *@size:Size of the array fo the hash table
+ *
+ *Return: Hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+
+
+
+}
