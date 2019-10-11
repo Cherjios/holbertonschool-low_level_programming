af6cf90c1c5b5226a8413d0afb3093ef4c506de1
Sergio Lopez <slopez0286@gmail.com>
2019-10-11 17:40:39 +0000

    Adding key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,14 +2,15 @@
 
 /**
  *key_index-Function that gives us the index of a key
- *@key: Key to look for it 
+ *@key: Key to look for it
  *@size:Size of the array fo the hash table
  *
  *Return: Hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-
-
+	if (!size)
+		return (0);
+	return (hash_djb2(key) % size);
 
 }
