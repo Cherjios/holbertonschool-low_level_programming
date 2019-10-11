8ddb85b2f92d21a3ecf557cde119319f43903bb8
Sergio Lopez <slopez0286@gmail.com>
2019-10-11 16:39:12 +0000

    Hash table create

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_t-Function that creates a hash table
+ *@size:Size the array hash table
+ *
+ *Return: Hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	if (!size)
+		return (NULL);
+
+	ht = calloc(1, sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_table_t *));
+	if (!ht->array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
+}
