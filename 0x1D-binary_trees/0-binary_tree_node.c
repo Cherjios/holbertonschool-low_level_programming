732a79dc5e815522d9e07b86f700a06280ded845
cherjiosclear <slopez0286@gmail.com>
2019-12-05 02:17:40 +0000

    Fixing 0

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -12,7 +12,8 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 
-binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+binary_tree_t *new_node = NULL;
+new_node = calloc(1, sizeof(binary_tree_t));
 
 if (!new_node)
 	return (NULL);
