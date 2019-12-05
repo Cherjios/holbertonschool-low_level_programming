b377d3311292e884bb93606b5dc66279c1af9535
cherjiosclear <slopez0286@gmail.com>
2019-12-05 20:02:10 +0000

    Fixing 5

--- 0x1D-binary_trees/5-binary_tree_is_root.c
+++ 0x1D-binary_trees/5-binary_tree_is_root.c
@@ -11,9 +11,7 @@ int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
-	if (node->left != NULL && node->right != NULL)
+	if (!node->parent)
 		return (1);
-	binary_tree_is_root(node->left);
-	binary_tree_is_root(node->right);
 	return (0);
 }
