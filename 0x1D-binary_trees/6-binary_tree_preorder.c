07cb40707d9eb038b16a91f10792af846f2a6264
cherjiosclear <slopez0286@gmail.com>
2019-12-05 23:16:42 +0000

    Adding 6 preorder

---
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder- Function that goes using pre-order traversal
+ *
+ * @tree: is a pointer to the root node
+ * @func: is a pointer to a function to call for each node
+ *
+ * Return: return 1 is node is a leaf, otherwise 0
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
