d1301a15d77e77ad5e57fd481121c07006a1646e
cherjiosclear <slopez0286@gmail.com>
2020-01-15 22:37:00 +0000

    Fixing file 0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -20,7 +20,7 @@ return (-1);
 
 for (i = 0; i < size; i++)
 {
-printf("Value checked array [%i] = [%i]\n", i, array[i]);
+printf("Value checked array[%i] = [%i]\n", i, array[i]);
 if (array[i] == value)
 return (i);
 }
