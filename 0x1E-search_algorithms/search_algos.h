4e9fff747c6e76f7b3796a5e24e13cdd9a68b44d
cherjiosclear <slopez0286@gmail.com>
2020-01-15 21:51:51 +0000

    Adding header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif /*_SEARCH_ALGOS*/
